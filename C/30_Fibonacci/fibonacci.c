6a239ea129c7602c813e5c232ce8c380f8aecd00
Lizzy <six_eight_squash@yahoo.com>
2025-07-06 20:25:56 -0400

    Capture Fibonacci example

---
+++ C/30_Fibonacci/fibonacci.c
@@ -0,0 +1,101 @@
+/*****************************************************************************
+* FILE: fibonacci.c
+*
+* DESCRIPTION:
+*    Implements a function to calculate Fibonacci numbers.  The function
+*    takes an index and calculates the corresponding Fibonacci number, using
+*    the index as the position of the number in the Fibonacci series.
+*    For example, given 'n', the Fibonacci numbers are:
+*
+*            n        Fibonacci
+*         ---------------------------
+*            0           -
+*            1           1
+*            2           2
+*            3           3 (2 + 1)
+*            4           5 (3 + 2)
+*            5           8 (5 + 3)
+*            6          13 (8 + 5)
+*
+*    Right back at ya, Dr DeVore ...
+*
+* COPYRIGHT:
+*    Copyright (c) 2016, Sally Recuas (Sally_Recuas@yahoo.com)
+*
+*    Permission to use, copy, modify, and/or distribute this software for any
+*    purpose with or without fee is hereby granted, provided that the above
+*    copyright notice and this permission notice appear in all copies.
+*
+*    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+*    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+*    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+*    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+*    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+*    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+*    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+******************************************************************************/
+
+#include <stdio.h>
+
+
+static int Fibonacci(const unsigned int n);
+
+
+/***************************************
+* FUNCTION: main
+*
+* DESCRIPTION:
+*    Main entry point of program.
+****************************************/
+int main (int argc, char **argv)
+
+{
+   int          f = -1;
+   unsigned int n = 0;
+
+   if (argc != 2)
+   {
+       printf("***> ERROR - invalid argument(s)\n"
+              "***> Usage:\n"
+              "***>    fibonacci <n>\n"
+              "***> Example:\n"
+              "***>    fibonacci 5\n");
+
+       return -1;
+   }
+
+   sscanf(argv[1], "%u", &n);
+
+   f = Fibonacci(n);
+
+   printf("---> The %uth Fibonacci number is...: %d\n", n, f);
+
+   return 0;
+}
+
+
+/***************************************
+* FUNCTION: Fibonacci
+*
+* DESCRIPTION:
+*    Calculates the nth Fibonacci number.
+****************************************/
+static int Fibonacci(const unsigned int n)
+
+{
+   int f;
+
+   if ( ( n == 0 ) ||
+        ( n == 1 ) ||
+        ( n == 2 ) ||
+        ( n == 3 ) )
+   {
+       return n;
+   }
+
+   f = Fibonacci(n - 1) + Fibonacci(n - 2);
+
+   return f;
+}
+
+/* EOF */
---
+++ C/30_Fibonacci/w32_build/run_fib.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set HERE=%cd%
+
+cd Debug
+
+fibonacci 5
+
+fibonacci 10
+
+fibonacci 15
+
+cd %HERE%
