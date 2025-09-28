38fcfe67f7e448720f2433171be5dbb4925d27e5
Lizzy <six_eight_squash@yahoo.com>
2025-09-28 18:31:01 -0400

    Tcl graphics demo of simple sine and cosine

---
+++ Tcl/04_sine_cosine/run_sin_cos.bat
@@ -0,0 +1,20 @@
+@echo off
+rem **************************************************************************
+
+rem * FILE: run_sin_cos.bat
+
+rem *
+
+rem * DESCRIPTION:
+
+rem *    DOS batch program to run the simple graphics script
+
+rem **************************************************************************
+
+set HERE=%cd%
+
+C:\Tcl\bin\wish sin_cos_graphics.tcl
+
+
+
+echo Done
---
+++ Tcl/04_sine_cosine/run_sin_cos.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# * *************************************************************************
+# * FILE: run_graphics.bat
+# *
+# * DESCRIPTION:
+# *    DOS batch program to run the simple graphics script
+# * *************************************************************************
+
+export HERE=`pwd`
+
+/usr/bin/wish sin_cos_graphics.tcl
+
+echo "Done"
