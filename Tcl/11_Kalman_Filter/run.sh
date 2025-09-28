690a5a8611c805ece0ea6db9c0e7447cd88be8c1
Lizzy <six_eight_squash@yahoo.com>
2025-09-28 19:57:50 -0400

    Attempting to implement a Kalman Filter
    At this point, this is just a copy of the 'sensor_plot_viewer.tcl'.
    
    Got a long way to go ...
    
    Changes to be committed:
    	new file:   kalman_filter.tcl
    	new file:   run.bat
    	new file:   run.sh

---
+++ Tcl/11_Kalman_Filter/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# * *************************************************************************
+# * FILE: run.sh
+# *
+# * DESCRIPTION:
+# *    Unix script to run the 'Kalman Filter' demo program.
+# * *************************************************************************
+
+/usr/bin/wish kalman_filter.tcl plot_list.01.txt
+
+echo "---> Done"
+
