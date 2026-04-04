2f7707403bac3d0030684c341735c6a19e025904
Lizzy <six_eight_squash@yahoo.com>
2026-04-04 19:23:52 -0400

    Implement 'exit' and 'draw grid' mechanics
    Just a start ... more to follow.
    
    I forgot the 'update()' in the drawGrid 'slot' function.
    
    Note:
    
        Can do:
           drawLine(x1, y1, x2, y2)
        or
           QLine line(x1, y1, x2, y2);
           drawLine(line)
    
    I goofed a bug and thought it was only the second one.  But both
    are OK.
    
    Changes to be committed:
    	modified:   basic_graphics.cpp
    	modified:   basic_graphics.h
    	modified:   basic_graphics.pro.user
    	modified:   basic_graphics.ui

--- Qt/04_basic_graphics/basic_graphics.cpp
+++ Qt/04_basic_graphics/basic_graphics.cpp
@@ -1,15 +1,90 @@
+//============================================================================
+// FILE: basic_graphics.cpp
+//
+// DESCRIPTION:
+//    Manages the 'basic-graphics' panel.
+//============================================================================
+#include <iostream>
+
+#include <QPainter>
+#include <QPaintEvent>
+
 #include "basic_graphics.h"
 #include "ui_basic_graphics.h"
 
+//========================================================
+// METHOD: basic_graphics_MainWindow 
+//
+// DESCRIPTION:
+//    Constructor as entry point to create the GUI/Window
+//    of the application.
+//========================================================
 basic_graphics_MainWindow::basic_graphics_MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::basic_graphics_MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->actionExitButton,
+            &QAction::triggered,
+            this,
+            &basic_graphics_MainWindow::exit_action_slot);
+
+    connect(ui->actionGridButton,
+            &QAction::triggered,
+            this,
+            &basic_graphics_MainWindow::draw_grid_action_slot);
 }
 
 basic_graphics_MainWindow::~basic_graphics_MainWindow()
 {
     delete ui;
 }
+
+void basic_graphics_MainWindow::exit_action_slot()
+{
+   std::exit(0);
+}
+
+void basic_graphics_MainWindow::draw_grid_action_slot()
+{
+   if (draw_grid)
+      draw_grid = false;
+   else
+      draw_grid = true;
+
+   std::cout << "Grid: " << draw_grid << "\n";
+
+   update();
+}
+
+void basic_graphics_MainWindow::paintEvent(QPaintEvent *event)
+{
+    if (!event)
+    {
+        std::cout << "ERROR - paint event is NULL\n";
+        return;
+    }
+
+    if (draw_grid)
+    {
+        QPainter painter(this);
+
+        x = ui->centralwidget->width() / 2;
+        y = ui->centralwidget->height();
+
+        const QLine x_axis(x, 0, x, y);
+
+        painter.drawLine(x_axis);
+
+        x = ui->centralwidget->width();
+        y = ui->centralwidget->height() / 2;
+
+        const QLine y_axis(0, y, x, y);
+
+        painter.drawLine(y_axis);
+    }
+}
+
+/* EOF */
 
--- Qt/04_basic_graphics/basic_graphics.h
+++ Qt/04_basic_graphics/basic_graphics.h
@@ -15,7 +15,27 @@ public:
     basic_graphics_MainWindow(QWidget *parent = nullptr);
     ~basic_graphics_MainWindow();
 
+public slots:
+
+   void exit_action_slot();
+
+   void draw_grid_action_slot();
+
+protected:
+
+   void paintEvent(QPaintEvent *event);
+
 private:
     Ui::basic_graphics_MainWindow *ui;
+
+    int x = 0;
+    int y = 0;
+
+    QColor pen_color = Qt::black;
+
+    bool draw_grid = false;
 };
 #endif // BASIC_GRAPHICS_MAINWINDOW_H
+
+/* EOF */
+
