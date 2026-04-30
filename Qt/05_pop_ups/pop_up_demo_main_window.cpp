dbeae37056b7664836d71e6c353bdce9e2903add
Lizzy <six_eight_squash@yahoo.com>
2026-04-29 23:10:37 -0400

    First Cut at Showing pop ups ...
    Stumbled on 'QDialog' class/widget ... using the 'Qt Assistant'
    (not in 'QtCreator').  So this pop ups empty dialog boxes.
    Not sure if this is progress ... because the next thing is,
    how 'design' a pop up window?  I may be going down the wrong
    hole.
    
    Changes to be committed:
    	modified:   pop_up_demo.pro.user
    	modified:   pop_up_demo_main_window.cpp

--- Qt/05_pop_ups/pop_up_demo_main_window.cpp
+++ Qt/05_pop_ups/pop_up_demo_main_window.cpp
@@ -6,8 +6,16 @@
 //============================================================================
 #include <iostream>
 
+#include <QDialog>
+
 #include "pop_up_demo_main_window.h"
 #include "ui_pop_up_demo_main_window.h"
+
+
+static QDialog *dialog_1 = NULL;
+static QDialog *dialog_2 = NULL;
+static QDialog *dialog_3 = NULL;
+
 
 pop_up_demo_Main_Window::pop_up_demo_Main_Window(QWidget *parent)
 
@@ -37,12 +45,25 @@ pop_up_demo_Main_Window::pop_up_demo_Main_Window(QWidget *parent)
 
 pop_up_demo_Main_Window::~pop_up_demo_Main_Window()
 {
+    if (dialog_1) delete dialog_1;
+    if (dialog_2) delete dialog_2;
+    if (dialog_3) delete dialog_3;
+
     delete ui;
 }
 
 void pop_up_demo_Main_Window::pop_up_1_action_slot()
 {
     std::cout << "pop up 1\n";
+
+    if ( ! dialog_1 )
+    {
+        dialog_1 = new QDialog(ui->centralwidget, Qt::Dialog);
+    }
+
+    dialog_1->setVisible(true);
+
+    dialog_1->show();
 }
 
 void pop_up_demo_Main_Window::exit_action_slot()
@@ -53,11 +74,29 @@ void pop_up_demo_Main_Window::exit_action_slot()
 void pop_up_demo_Main_Window::pop_up_2_action_slot()
 {
     std::cout << "pop up 2\n";
+
+    if ( ! dialog_2 )
+    {
+        dialog_2 = new QDialog(ui->centralwidget, Qt::Dialog);
+    }
+
+    dialog_2->setVisible(true);
+
+    dialog_2->show();
 }
 
 void pop_up_demo_Main_Window::on_pop_up_3_pushButton_clicked()
 {
     std::cout << "pop up pushButton\n";
+
+    if ( ! dialog_3 )
+    {
+        dialog_3 = new QDialog(ui->centralwidget, Qt::Dialog);
+    }
+
+    dialog_3->setVisible(true);
+
+    dialog_3->show();
 }
 
 /* EOF */
